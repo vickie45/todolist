d7830463ff71869b4b0d547538ec5b1dc6f05936
vickie45 <s.v.nadar45@gmail.com>
2020-01-11 00:02:42 +0530

    added backend code

---
+++ backend/models/task.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
